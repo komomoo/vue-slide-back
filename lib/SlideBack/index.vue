e877ab2de7e59e7e3b8ccd655229589620469415
momoko <928701977@qq.com>
2018-08-08 18:27:35 +0800

    init commit

---
+++ lib/SlideBack/index.vue
@@ -0,0 +1,41 @@
+/**
+ * vue-slide-back
+ * @Author momoko
+ * @Date 2018/08
+ */
+
+<template>
+  <div :class="c()">
+    123
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import mixin from './mixins'
+
+export default {
+  name: 'SlideBack',
+  components: {},
+  mixins: [mixin],
+  props: {
+  },
+  data () {
+    return {}
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    // 初始化
+    init () {},
+  },
+}
+</script>
+
+<style lang="stylus">
+$ = vue-slide-back;
+
+.{$} {
+
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <SlideBack/>
+  </div>
+</template>
+
+<script>
+import SlideBack from '../lib'
+
+export default {
+  name: 'App',
+  components: {
+    SlideBack,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="stylus">
+$headerHeight = 44px;
+$baseColor = #6A9FB5;
+$bgColor = #FAFAFA;
+* {
+  padding: 0;
+  margin: 0;
+  border: none;
+}
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  overflow-y: scroll;
+  box-sizing: border-box;
+}
+</style>
